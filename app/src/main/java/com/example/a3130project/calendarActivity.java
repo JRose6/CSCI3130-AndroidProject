211dffc3a911b9a7910511ce3b0b580b06f59b36
Yichen Zhao <yc510564@dal.ca>
2019-06-28 12:44:31 -0300

    Calendar successfully shows, working on adding medication information [Yichen,Chichi]

--- app/src/main/java/com/example/a3130project/calendarActivity.java
+++ app/src/main/java/com/example/a3130project/calendarActivity.java
@@ -1,16 +1,32 @@
 package com.example.a3130project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CalendarView;
 
 public class calendarActivity extends AppCompatActivity
 {
-
+	private  static final String TAG = "CalendarActivity";
+	private CalendarView mCalendarView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_calendar);
+		mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+		mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+			@Override
+			public void onSelectedDayChange(CalendarView CalendarView, int year, int month, int dayOfMonth) {
+				String date = year + "/" + month + "/"+ dayOfMonth ;
+				Log.d(TAG, "onSelectedDayChange: yyyy/mm/dd:" + date);
+				Intent intent = new Intent(calendarActivity.this, MainActivity.class);
+				intent.putExtra("date",date);
+				startActivity(intent);
+
+			}
+		});
 	}
 }
+
