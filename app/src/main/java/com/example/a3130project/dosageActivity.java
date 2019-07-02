66f4eecc30c7a67256471465b788c3e7b069f2f9
EmilioRoche <32501779+EmilioRoche@users.noreply.github.com>
2019-07-02 10:13:35 -0300

    (ADD)[Jordan,Emilio] added a list for dosage

--- app/src/main/java/com/example/a3130project/dosageActivity.java
+++ app/src/main/java/com/example/a3130project/dosageActivity.java
@@ -3,14 +3,37 @@ package com.example.a3130project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class dosageActivity extends AppCompatActivity
 {
+
+	private ListView listviewMed;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dosage);
+
+		listviewMed = findViewById(R.id.listViewMed);
+
+		ArrayList<String> arr = new ArrayList<>();
+
+		arr.add("Advil");
+		arr.add("Ibuprofen");
+		arr.add("Addrerall");
+		arr.add("Lisinopril");
+		arr.add("Hydrocodone");
+
+
+		ArrayAdapter arrAdap = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arr);
+
+		listviewMed.setAdapter(arrAdap);
+
 	}
+
 }
