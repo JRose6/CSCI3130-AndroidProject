9accd0f88e65b69f2b5bdd9034ca4292540f2326
EmilioRoche <emiliosroche@gmail.com>
2019-07-14 10:24:03 -0300

    [Yichen,Emilio](REF) fixed xml template

--- app/src/main/java/com/example/a3130project/EmployeeProfileActivity.java
+++ app/src/main/java/com/example/a3130project/EmployeeProfileActivity.java
@@ -3,19 +3,30 @@ package com.example.a3130project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class EmployeeProfileActivity extends AppCompatActivity
 {
-
+	private EditText medName, normName, mainDiseases, manufact, sideEff;
+	private Button addMedi;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_employee_profile);
 
+		medName = findViewById(R.id.genNameEmp);
+		normName = findViewById(R.id.nameEmp);
+		mainDiseases = findViewById(R.id.mainDiseasesEmp);
+		manufact = findViewById(R.id.manuEmp);
+		sideEff = findViewById(R.id.sideEffectsEmp);
+
+		addMedi = findViewById(R.id.addMedEmp);
+
 		ToolBarCreator.createBottomNav(this);
 		ToolBarCreator.createToolbar(this, true, false);
-		//'button'
+
 	}
 }
