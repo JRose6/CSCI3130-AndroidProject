3d563f8571e663f7ab31c1f3fd6b10afe0807b1c
Yichen Zhao <yc510564@dal.ca>
2019-07-14 11:35:56 -0300

    [Yichen,Emilio](ADD) Array input testing in employee page

--- app/src/main/java/com/example/a3130project/EmployeeProfileActivity.java
+++ app/src/main/java/com/example/a3130project/EmployeeProfileActivity.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 
 import com.example.a3130project.Helpers.ToolBarCreator;
 import com.example.a3130project.model.Profile;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Arrays;
 
 
 public class EmployeeProfileActivity extends AppCompatActivity
@@ -42,6 +46,11 @@ public class EmployeeProfileActivity extends AppCompatActivity
 		addMedi = findViewById(R.id.addMedEmp);
 
 
+		String mainDiseases1 = mainDiseases.getText().toString();
+		String diseases[] = mainDiseases1.split(",");
+
+
+
 		database = FirebaseFirestore.getInstance();
 
 		mAuth = FirebaseAuth.getInstance();
@@ -54,7 +63,22 @@ public class EmployeeProfileActivity extends AppCompatActivity
 
 		//ToolBarCreator.createToolbar(this, true, false);
 
+		addMedi.setOnClickListener(new OnClicker());
 
-		Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+		String TEST = Arrays.toString(diseases);
+		Toast.makeText(this, TEST, Toast.LENGTH_LONG).show();
+
+	}
+
+	public class OnClicker implements View.OnClickListener
+	{
+		@Override
+		public void onClick(View v)
+		{
+			String mainDiseases1 = mainDiseases.getText().toString();
+			String diseases[] = mainDiseases1.split(",");
+			String TEST = Arrays.toString(diseases);
+			Toast.makeText(EmployeeProfileActivity.this, TEST, Toast.LENGTH_SHORT).show();
+		}
 	}
 }
